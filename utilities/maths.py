06cc9ab15e07b4b8133b166bbab2d550763179b4
Prasad <psam012@aucklanduni.ac.nz>
2018-11-28 15:28:14 +1300

    Adding transform rigid 3D function

--- utilities/maths.py
+++ utilities/maths.py
@@ -13,3 +13,29 @@ def sph2cart(azimuth,elevation,r):
     y = r * np.cos(elevation) * np.sin(azimuth)
     z = r * np.sin(elevation)
     return x, y, z
+
+
+def transformRigid3D(x, t):
+    """ applies a rigid transform to list of points x.
+    T = (tx,ty,tz,rx,ry,rz)
+    """
+    X = np.vstack((x.T, np.ones(x.shape[0])))
+    T = np.array([[1.0, 0.0, 0.0, t[0]],
+                  [0.0, 1.0, 0.0, t[1]],
+                  [0.0, 0.0, 1.0, t[2]],
+                  [1.0, 1.0, 1.0, 1.0]])
+
+    Rx = np.array([[1.0, 0.0, 0.0],
+                   [0.0, np.cos(t[3]), -np.sin(t[3])],
+                   [0.0, np.sin(t[3]), np.cos(t[3])]])
+
+    Ry = np.array([[np.cos(t[4]), 0.0, np.sin(t[4])],
+                   [0.0, 1.0, 0.0],
+                   [-np.sin(t[4]), 0.0, np.cos(t[4])]])
+
+    Rz = np.array([[np.cos(t[5]), -np.sin(t[5]), 0.0],
+                   [np.sin(t[5]), np.cos(t[5]), 0.0],
+                   [0.0, 0.0, 1.0]])
+
+    T[:3, :3] = np.dot(np.dot(Rx, Ry), Rz)
+    return np.dot(T, X)[:3, :].T
